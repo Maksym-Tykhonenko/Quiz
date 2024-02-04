9057a12220dbc5bd206f51c69c40b0ea94a1d793
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2024-02-04 11:08:53 +0200

    save

---
+++ quiz/data/germanHotels.js
@@ -0,0 +1,103 @@
+export const germanHotels = [
+    {
+        id: 1,
+        hotel: `Maritim Berlin`,
+        location: `Friedrichstraße 151 Dorotheenstraße 65 (Navigationsadresse, 10117 Berlin`, //
+        description: `This hotel in Berlin features a casino offering various gaming options for guests.`,
+        numberOfRooms: 349,
+        photo: require('../assets/german/MaritimBerlin.jpeg'),
+        latitude: 52.518340754829275, 
+        longitude: 13.387160008697418,
+    },
+    {
+        id: 2,
+        hotel: `Adlon Kempinski`,
+        location: `Unter den Linden 77, 10117 Berlin`, //
+        description: `Located in the heart of Berlin, near the Brandenburg Gate, Hotel Adlon Kempinski is an iconic luxury hotel known for its historical significance and opulent accommodations. With stunning views of the city, exceptional dining options, and world-class service, it embodies elegance and sophistication.`,
+        numberOfRooms: 164,
+        photo: require('../assets/german/AdlonKempinski.jpeg'),
+        latitude: 52.51601653914004, 
+        longitude: 13.379944154728385,
+    },
+
+    {
+        id: 3,
+        hotel: `Brenners Park-Hotel`,
+        location: `Ludwig-Wilhelm-Platz 4, 76530 Baden-Baden`, //
+        description: `Nestled in Baden-Baden amidst picturesque surroundings, Brenners Park-Hotel & Spa is a haven of tranquility and luxury. Its renowned spa facilities, gourmet dining experiences, and refined accommodations offer guests a lavish retreat.`,
+        numberOfRooms: 81,
+        photo: require('../assets/german/BrennersParkHotel.jpeg'),
+        latitude: 48.75730423495118, 
+        longitude: 8.240588466280995,
+    },
+    {
+        id: 4,
+        hotel: 'Bayerischer Hof',
+        location: 'Promenadepl. 2-6, 80333 München',//
+        description: `Situated in Munich, Bayerischer Hof is a historic hotel blending tradition with contemporary luxury. Featuring exquisite restaurants, elegant rooms, and a rooftop bar offering panoramic views, it is a symbol of sophistication in the city.`,
+        numberOfRooms: 290,
+        photo: require('../assets/german/BayerischerHof.jpeg'),
+        latitude: 48.14039506637245,
+        longitude: 11.573375050333992,
+    },
+    {
+        id: 5,
+        hotel: 'Hotel Nassauer Hof',
+        location: 'Kaiser-Friedrich-Platz 3-4, 65183 Wiesbaden',//
+        description: `Found in Wiesbaden, Hotel Nassauer Hof boasts a rich history and modern amenities. Its spa, gourmet restaurants, and classic ambiance make it a favored choice for discerning travelers seeking elegance and comfort.`,
+        numberOfRooms: 115,
+        photo: require('../assets/german/HotelNassauerHof.jpeg'),
+        latitude: 50.08492850960566,
+        longitude:  8.244126952252595,
+    },
+    {
+        id: 6,
+        hotel: 'Rocco Forte Villa',
+        location: 'Kennedyallee 70, 60596 Frankfurt am Main',//
+        description: `In Frankfurt, Rocco Forte Villa Kennedy combines modern design with timeless luxury. Its lush gardens, wellness facilities, and impeccable service create a peaceful urban oasis for guests.`,
+        numberOfRooms: 331,
+        photo: require('../assets/german/RoccoForteVilla.jpeg'),
+        latitude: 50.09719198706527,
+        longitude:  8.669481436920428,
+    },
+    {
+        id: 7,
+        hotel: 'Spa & Cultural Hideaway',
+        location: 'In Elmau 2, 82493 Krün',//
+        description:`Tucked away in the Bavarian Alps, Schloss Elmau offers a unique blend of luxury, culture, and nature. Surrounded by stunning landscapes, it hosts a world-class spa and diverse cultural programs.`,
+        numberOfRooms: 125,
+        photo: require('../assets/german/SpaCulturalHideaway.jpeg'),
+        latitude: 47.461968098273694, 
+        longitude: 11.18652846502244,
+    },
+    {
+        id: 8,
+        hotel: 'Villa Stéphanie',
+        location: 'Ludwig-Wilhelm-Platz 4, 76530 Baden-Baden',//
+        description: `A wellness resort in Baden-Baden, Villa Stéphanie offers exclusive spa treatments, state-of-the-art facilities, and personalized service, catering to guests' well-being and relaxation.`,
+        numberOfRooms: 89,
+        photo: require('../assets/german/VillaStphanie.jpeg'),
+        latitude: 48.725147966825595, 
+        longitude: 8.283564823115356,
+    },
+    {
+        id: 9,
+        hotel: `Berlin Hackescher Markt`,
+        location: 'Hackescher Markt, 10178 Berlin',//
+        description: `Situated in Berlin's vibrant Hackescher Markt area, this contemporary hotel provides spacious apartments with modern amenities, catering to both business and leisure travelers exploring the city.`,
+        numberOfRooms: 200,
+        photo: require('../assets/german/BerlinHackescherMarkt.jpeg'),
+        latitude: 52.52363712144869, 
+        longitude: 13.402310770080765,
+    },
+    {
+        id: 10,
+        hotel: 'Sofitel Munich Bayerpost',
+        location: 'Bayerstraße 12, 80335 München',//
+        description: `Housed in Munich's historic post office building, Sofitel Munich Bayerpost combines heritage with modern luxury. Its chic design, fine dining options, and central location offer a sophisticated experience in the city.`,
+        numberORoms: 252,
+        photo: require('../assets/german/SofitelMunichBayerpost.jpeg'),
+        latitude: 48.139401665262326, 
+        longitude: 11.556738334988568,
+    }
+];
--- quiz/screens/LvlGermany.js
+++ quiz/screens/LvlGermany.js
@@ -1,24 +1,180 @@
-import React,{useState,useRef,useEffect} from 'react';
-import {
-  ImageBackground,
-  Text,
-    View,
-} from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import {View, Text, ImageBackground, TouchableOpacity, ScrollView, SafeAreaView, Modal, TextInput , Image} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+import { germanHotels } from '../data/germanHotels';
 
 const LvlGermany = ({ navigation }) => {
+    const [startMassiv, setStartMassiv] = useState(germanHotels);
+    const [sights, setSights] = useState(startMassiv);
+    console.log('sights', sights.length);
+    const [sightsImg, setSightsImg] = useState(startMassiv);
+    //console.log('sightsImg', sightsImg.length)
+    const [shuffledSightsImg, setShuffledSightsImg] = useState([]);
+    //console.log('length==>', shuffledSightsImg.length)
+
+    const [actButnTitle, setActButnTitle] = useState(-1);
+    const [actBtnImg, setActBtnImg] = useState(-1);
+    //console.log('==>', actButnTitle, actBtnImg)
+    const [btnIsVisible, setBtnIsVisible] = useState(false);
+    const [quizIsComplited, setQuizIsComplited] = useState(false);
+
+    const goTooAppAfterQuizComplited = () => {
+        setBtnIsVisible(false);
+        navigation.navigate('QuizScreen')
+    };
+
+    useEffect(() => {
+        setBtnIsVisible(false)
+        if (shuffledSightsImg.length === 0) {
+            setBtnIsVisible(true)
+        }
+    }, [shuffledSightsImg]);
+
+    // Функція для перемішування масиву
+    const shuffleArray = (array) => {
+        const newArray = [...array]; // Створюємо копію масиву
+        for (let i = newArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)); // Генеруємо випадкове число
+            [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Переставляємо елементи масиву
+        }
+        return newArray; // Повертаємо перемішаний масив
+    };
+
+    // Функція для перемішування масиву sightsImg перед рендерингом
+    useEffect(() => {
+        setShuffledSightsImg(shuffleArray(sights));
+    }, [sights]);
+
+    // Функція для зміни кольору в кнопці з імедж
+    const changeColorInImg = (id) => {
+        if (id === actBtnImg && id === actButnTitle) {
+            return 'green';
+        }
+        return '#f5c65d'
+    };
+
+    {/** */ }
+    // Функція для видалення об'єкта за його id
+    const removeSightById = (id) => {
+        const updatedSights = sights.filter(item => item.id !== id);
+        //setStartMassiv(updatedSights)
+        setSights(updatedSights);
+        setShuffledSightsImg(updatedSights);
+        setActButnTitle(-1)
+        setActBtnImg(-1)
+    };
+
+    // Викликати цю функцію всередині useEffect, щоб видалити елемент з масиву після зміни стану
+    useEffect(() => {
+        if (actButnTitle !== -1 && actButnTitle === actBtnImg) {
+            removeSightById(actButnTitle);
+        }
+    }, [actButnTitle, actBtnImg]);
     return (
         <View style={{ flex: 1, }}>
 
             <ImageBackground
                 source={require('../assets/bgrQuiz.jpeg')}
                 style={{ flex: 1 }}>
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    
-                    <View>
-                        <Text style={{ color:'gold', fontSize: 30}}>Germany</Text> 
+                
+                <SafeAreaView style={{ flex: 1, position: 'relative', alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity
+                            onPress={() => {  }}
+                            style={{  alignItems: 'center', justifyContent: 'center', backgroundColor: '#f5c65d', width: 250, height: 80, borderWidth: 2, borderColor: '#f5c65d', borderRadius: 10 }}>
+                            
+                            <Text style={{ color: '#000', fontWeight: 'bold', fontSize: 30 }}>TAIMER</Text>
+                            </TouchableOpacity>
+                    <View style={{ flex: 1, position: 'relative', marginTop: 30, marginBottom: 10, flexDirection: 'row', justifyContent: 'space-around' }}>
+
+                        
+
+                        {/* Блок кнопок з назвами */}
+                        <View style={{ flex: 0.5, alignItems: 'center' }}>
+                            <Text style={{ marginLeft: 20, marginBottom: 10, color: '#f5c65d', fontWeight: 'bold', fontSize: 20 }}>Categories: </Text>
+                        
+                            <ScrollView>
+                            
+                                {/* Блок кнопок з назвами */}
+                                {sights.map((item, index) => (
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            setActButnTitle(item.id);
+                                        }}
+                                        style={{
+                                            marginBottom: 10,
+                                            width: 150,
+                                            height: 135,
+                                            borderWidth: 1,
+                                            borderRadius: 15,
+                                            backgroundColor: actButnTitle === item.id ? 'green' : '#0395e0',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            padding: 5,
+                                            borderColor: actButnTitle === item.id ? 'green' : 'gold',
+                                        }}
+                                        key={item.id}
+                                    >
+                                        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{item.hotel}</Text>
+                                    </TouchableOpacity>
+                                ))}
+                            </ScrollView>
+                        </View>
+
+                        {/* Полоска */}
+                        <View style={{ borderWidth: 1, borderColor: 'red' }}></View>
+
+                        {/* Блок зображень */}
+                        <View style={{ flex: 0.5, alignItems: 'center' }}>
+
+                            <Text style={{ marginBottom: 10, color: '#f5c65d', fontWeight: 'bold', fontSize: 20 }}>Photos: </Text>
+
+                            <ScrollView>
+              
+                                {/* Блок зображень */}
+                                {shuffledSightsImg.map((item) => (
+                                    <TouchableOpacity
+                                        onPress={() => { setActBtnImg(item.id) }} // Обробник події натискання на зображення
+                                        style={{
+                                            marginBottom: 10,
+                                            borderWidth: 8,
+                                            borderRadius: 15,
+                                            borderColor: changeColorInImg(item.id)
+                                        }}
+                                        key={item.id}
+                                    >
+                                        <Image style={{ width: 150, height: 119, borderRadius: 10 }} source={item.photo} />
+                                    </TouchableOpacity>
+                                ))}
+                            </ScrollView>
+
+                        </View>
+                   
+                        {/* Кнопка перехода на Home Screen */}
+                        {btnIsVisible && <TouchableOpacity
+                            onPress={() => { goTooAppAfterQuizComplited() }}
+                            style={{ position: 'absolute', top: '50%', right: 55, alignItems: 'center', justifyContent: 'center', backgroundColor: '#f5c65d', width: 250, height: 80, borderWidth: 2, borderColor: '#f5c65d', borderRadius: 10 }}>
+                            
+                            <Text style={{ color: '#000', fontWeight: 'bold', fontSize: 18 }}>Congratulations!!!</Text>
+                            <Text style={{ color: '#000', fontWeight: 'bold', fontSize: 18 }}> Go to next Lvl  <Entypo name='arrow-bold-right' style={{ fontSize: 20 }} /></Text>
+                        </TouchableOpacity>}
+                
                     </View>
+
+
                     
-                </View>
+
+
+
+
+                    {/**Go Back BTN */}
+                    <TouchableOpacity
+                        onPress={() => { navigation.goBack() }}
+                        style={{ position: 'absolute', bottom: 15, right: 15, borderWidth: 3, borderRadius: 10, height: 60, width: 60, justifyContent: "center", alignItems: "center", borderColor: '#f5c65d', }}>
+                        <Text style={{ color: '#f5c65d', fontWeight: '600' }}>Go</Text>
+                        <Text style={{ color: '#f5c65d', fontWeight: '600' }}>back</Text>
+                    </TouchableOpacity>
+                </SafeAreaView>
                 
             </ImageBackground>
 
